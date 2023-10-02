363a197f322242b005384b769971c51de94fd1f0
Arilson Souza <arilsons61@gmail.com>
2023-10-02 12:49:09 -0300

    feat: add exception handler

--- first-web-api/src/main/java/com/dio/web/api/controller/UsuarioController.java
+++ first-web-api/src/main/java/com/dio/web/api/controller/UsuarioController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dio.web.api.handler.BusinessException;
 import com.dio.web.api.model.Usuario;
 import com.dio.web.api.repository.UsuarioRepository;
 
@@ -41,5 +43,14 @@ public class UsuarioController {
     @PostMapping
     public void createUser(@RequestBody Usuario usuario) {
         usuarioRepository.save(usuario);
+    }
+
+    @PutMapping
+    public void updateUser(@RequestBody Usuario usuario) {
+        if (usuario.getLogin() == null) {
+            throw new BusinessException("login field is required");
+        }
+
+        usuarioRepository.update(usuario);
     }
 }

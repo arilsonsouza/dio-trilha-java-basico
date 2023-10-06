9ac3b4121c58a09e3309e41af672610d0d6e5288
Arilson Souza <arilsons61@gmail.com>
2023-10-05 21:40:06 -0300

    feat: setup user entity

---
+++ jwt-security/src/main/java/com/dio/jwtsecurity/user/User.java
@@ -0,0 +1,27 @@
+package com.dio.jwtsecurity.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}

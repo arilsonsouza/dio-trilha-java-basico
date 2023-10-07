a38de5175094a8346a2567a29995f03644fc3b8d
Arilson Souza <arilsons61@gmail.com>
2023-10-07 10:09:42 -0300

    feat: add user repository

---
+++ santander-bootcamp-2023/src/main/java/me/dio/santanderbootcamp2023/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package me.dio.santanderbootcamp2023.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.santanderbootcamp2023.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}

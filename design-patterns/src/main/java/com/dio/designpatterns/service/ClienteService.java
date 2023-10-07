5bf9ae2a65fbdf363988ffd863adc5ccfe464d91
Arilson Souza <arilsons61@gmail.com>
2023-10-06 21:11:18 -0300

    feat: add spring boot design patterns project sample

---
+++ design-patterns/src/main/java/com/dio/designpatterns/service/ClienteService.java
@@ -0,0 +1,15 @@
+package com.dio.designpatterns.service;
+
+import com.dio.designpatterns.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
---
+++ design-patterns/src/test/java/com/dio/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
